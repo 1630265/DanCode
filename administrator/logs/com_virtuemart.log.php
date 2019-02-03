009abea89d7fbc0776435791064b93dea8e557dc
1630265 <1630265@upv.edu.mx>
2019-02-03 07:43:07 +0000

    Página versión 1: 10 categorías 2 productos

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,13 @@
+#
+#<?php die("Forbidden."); ?>
+
+2019-01-31 14:22:24 ERROR vmError: Save failed with the following error: This email address is already registered.
+2019-01-31 14:45:07 ERROR vmError: 10565: The merchant country is not supported.&lt;br /&gt;
+2019-02-03 02:12:07 ERROR vmError: Invalid media, image type does not fit to extension android1.png 2!=png
+2019-02-03 02:12:38 ERROR vmError: Invalid media, image type does not fit to extension android1.png 2!=png
+2019-02-03 02:13:07 ERROR vmError: Invalid media, image type does not fit to extension android2.png 2!=png
+2019-02-03 02:13:48 ERROR vmError: Invalid media, image type does not fit to extension android2.png 2!=png
+2019-02-03 02:14:47 ERROR vmError: Invalid media, image type does not fit to extension android2.png 2!=png
+2019-02-03 03:21:14 ERROR vmError: Invalid media, image type does not fit to extension plsql.jpg 3!=jpg
+2019-02-03 03:21:31 ERROR vmError: Invalid media, image type does not fit to extension plsql.jpg 3!=jpg
+2019-02-03 06:57:14 ERROR vmError: Vendor image given image is not complete images/joomlashine/placeholder/stories/virtuemart/vendor/.
---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-30 14:06:31 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-30T14:06:31+00:00	INFO 200.68.140.11	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-31T06:12:46+00:00	INFO 189.218.221.79	joomlafailure	Username and password do not match or you do not have an account yet.
